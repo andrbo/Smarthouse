688cab600eea484d412a953ef7f4ffec29dcb565
marcouadro <markus.markussen@hotmail.com>
2017-02-15 17:01:02 +0100

    t

--- router/home.js
+++ router/home.js
@@ -2,7 +2,6 @@ var express = require('express');
 var gpio = require('rpi-gpio');
 var router = express.Router();
 
-gpio.setup(15, gpio.DIR_OUT, write);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,6 +10,7 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/ledOn', function(req,res,next){
+    gpio.setup(15, gpio.DIR_OUT, write);
     function write() {
         gpio.write(15, true, function (err) {
             if (err) throw err;
@@ -20,6 +20,7 @@ router.get('/ledOn', function(req,res,next){
 });
 
 router.get('/ledOff', function(req,res,next){
+    gpio.setup(15, gpio.DIR_OUT, write);
     function write() {
         gpio.write(15, false, function (err) {
             if (err) throw err;
