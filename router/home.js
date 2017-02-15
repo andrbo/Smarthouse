deed9cefe26e856b717066b9c3d8c5dbf4dfd93a
marcouadro <markus.markussen@hotmail.com>
2017-02-15 15:05:21 +0100

    home

--- router/home.js
+++ router/home.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-//var gpio = require('rpi-gpio');
+var gpio = require('rpi-gpio');
 
 var bodyParser = require('body-parser');
 
@@ -12,8 +12,8 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
-//var dbModel = require('../models/home');
 
+var dbModel = require('../models/home');
 
 
 /* GET home page. */
@@ -22,27 +22,31 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/', function(req,res,next){
+router.get('/ledOn', function(req,res,next){
     gpio.setup(15, gpio.DIR_OUT, write);
 
     function write() {
         gpio.write(15, true, function (err) {
             if (err) throw err;
-            console.log('Written to pin');
+            console.log('led turned on');
         });
     }
 
     res.send(write());
-})
-
-
-/*router.post('/', function (req, res) {
-
-    var name = 'Mikael'
-    console.log(name);
-    dbModel.createUser(name), function (data) {
-        res.render('/home');
-    };
 });
-*/
-module.exports = router;
+
+router.get('/ledOff', function(req,res,next){
+    gpio.setup(15, gpio.DIR_OUT, write);
+
+    function write() {
+        gpio.write(15, false, function (err) {
+            if (err) throw err;
+            console.log('led turned off');
+        });
+    }
+
+    res.send(write());
+});
+
+
+module.exports = router
