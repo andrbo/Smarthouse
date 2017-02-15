6fc9adb527284bd26933daa9359f5b44bc9d32f9
marcouadro <markus.markussen@hotmail.com>
2017-02-15 15:24:40 +0100

    t

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-//var gpio = require('rpi-gpio');
+var gpio = require('rpi-gpio');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
