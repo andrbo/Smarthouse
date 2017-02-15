dddf4be8bb1d1234a34b33ffd20c16fc9f034b16
marcouadro <markus.markussen@hotmail.com>
2017-02-15 16:32:30 +0100

    t

--- router/home.js
+++ router/home.js
@@ -1,19 +1,6 @@
 var express = require('express');
-
 var gpio = require('rpi-gpio');
-
-var bodyParser = require('body-parser');
-
 var router = express.Router();
-
-var app = express();
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-}));
-
-var dbModel = require('../models/home');
 
 
 /* GET home page. */
@@ -42,8 +29,6 @@ router.get('/ledOff', function(req,res,next){
             console.log('led turned off');
         });
     }
-
-    res.send(write());
 });
 
 
