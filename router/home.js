4b77d306a1a872123b1beab14596bdd82a793968
marcouadro <markus.markussen@hotmail.com>
2017-02-16 14:38:20 +0100

    te

--- router/home.js
+++ router/home.js
@@ -15,13 +15,13 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/ledToggle', function(res, req, next){
-    console.log(req.query.button);
     gpio.setup(15, gpio.DIR_OUT, write);
 
     console.log("Før if");
 
     var ledOn = false;
-    var input = JSON.stringify(req.query.button);
+    var input = JSON.stringify(req.query);
+    console.log(input);
 
     if(input == "on"){
 
