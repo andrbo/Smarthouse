e5c6072261375a9d77824b748216df00e1acfa9f
anbors <andreasborsheim91@gmail.com>
2017-04-24 14:12:46 +0200

    Backend for styring av lys

--- middlewares/lightControl.js
+++ middlewares/lightControl.js
@@ -1,38 +1,36 @@
-//var rfTransmitter = require('nexa');
+var rfTransmitter = require('nexa');
 
 //Transmitter module is connected to wiringPi pin 15
-//rfTransmitter.nexaInit(15, function() {
-//    console.info("RF transmitter initialized");
-//});
+rfTransmitter.nexaInit(15, function() {
+    console.info("RF transmitter initialized");
+});
 
 module.exports = function (io) {
 
     io.sockets.on('connection', function (socket) {
+        var remote = 23328130;
 
-
-        socket.on('lyspaa', function () {
-            console.log('nexa påscript kjøres');
-            rfTransmitter.nexaOn(controller_id,device, function(){
-                console.log('lys paa');
-            });
-        });
-
-        socket.on('lightOff', function(data){
-            console.log('nexa avscript kjøres');
-            var controller_id = 23328130;
-            var unitId = JSON.stringify(data.unitno).trim();
-            rfTransmitter.nexaOff(controller_id,device, function(){
-                console.log('lys av');
-            });
-        });
-
-        socket.on('pairDevice', function(data){
-
-            var remote = 23328130;
+        socket.on('deviceOn', function () {
             var unit = JSON.stringify(data.unitno).trim();
-            // rfTransmitter.nexaParing(remote,unit, function() {
-            //    console.log("Done paring unit with remote: "+remote + "and unit code: "+unit);
-           // });)
+            rfTransmitter.nexaOn(remote, unit, function () {
+                console.log('Enhet: ' + unit + " på");
+            });
         });
+
+        socket.on('deviceOff', function (data) {
+            var unit = JSON.stringify(data.unitno).trim();
+            rfTransmitter.nexaOff(remote, unit, function () {
+                console.log('Enhet: ' + unit + " av");
+            });
+        });
+
+        socket.on('pairDevice', function (data) {
+            var unit = JSON.stringify(data.unitno).trim();
+            rfTransmitter.nexaParing(remote,unit, function() {
+                console.log("Done paring unit with remote: "+remote + "and unit code: "+unit);
+             });
+        });
+
     });
+
 };
