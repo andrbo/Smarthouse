409414a438898b04144b0542bdb2154414acd9bc
marcouadro <markus.markussen@hotmail.com>
2017-05-11 11:41:27 +0200

    rpi

--- public/js/home/information.js
+++ public/js/home/information.js
@@ -33,69 +33,71 @@ socket.on('serialEvent', function (data) {
     }
 }*/
 
-// Function for reading the soil moisture value from the Arduino.
-function laserReading(data) {
-    var laser = data.Laser;
-    var laserAlert = $("#laserAlarm").html();
-    var content = '<tr id="laserAlert" class="bg-danger"><td>' + laserAlert + '</td>' + '</tr>';
-    if (laser == 1 && !$("#laserAlert").length) {
-        $('#sensorWarningsTable tbody').append(content);
-        $('#sensorWarningGlyphicon').addClass("glyphicon-remove").css({"color": "red", "font-size": "100px", "opacity": "0.8"}).removeClass("glyphicon-ok");
-        $('#noWarningsHeader').hide();
-    }
+$(function () {
+    // Function for reading the soil moisture value from the Arduino.
+    function laserReading(data) {
+        var laser = data.Laser;
+        var laserAlert = $("#laserAlarm").html();
+        var content = '<tr id="laserAlert" class="bg-danger"><td>' + laserAlert + '</td>' + '</tr>';
+        if (laser == 1 && !$("#laserAlert").length) {
+            $('#sensorWarningsTable tbody').append(content);
+            $('#sensorWarningGlyphicon').addClass("glyphicon-remove").css({"color": "red", "font-size": "100px", "opacity": "0.8"}).removeClass("glyphicon-ok");
+            $('#noWarningsHeader').hide();
+        }
 
-    if(laser == 0){
-        $('#sensorWarningsTable #laserAlert').remove();
-        noErrors();
+        if(laser == 0){
+            $('#sensorWarningsTable #laserAlert').remove();
+            noErrors();
+        }
     }
-}
 
 // Function used for updating the value for the flame sensor
-function flameReading(data) {
-    var flameSensorValue = data.Flame;
-    var flameAlert = $("#flameAlarm").html();
-    var content = '<tr id="flameAlert" class="bg-danger"><td>' + flameAlert + '</td>' + '</tr>';
-    if (flameSensorValue == 0 && !$("#flameAlert").length) {
-        $('#sensorWarningsTable tbody').append(content);
-        $('#sensorWarningGlyphicon').addClass("glyphicon-remove").css({"color": "red", "font-size": "100px", "opacity": "0.8"}).removeClass("glyphicon-ok");
-        $('#noWarningsHeader').hide();
-    }
+    function flameReading(data) {
+        var flameSensorValue = data.Flame;
+        var flameAlert = $("#flameAlarm").html();
+        var content = '<tr id="flameAlert" class="bg-danger"><td>' + flameAlert + '</td>' + '</tr>';
+        if (flameSensorValue == 0 && !$("#flameAlert").length) {
+            $('#sensorWarningsTable tbody').append(content);
+            $('#sensorWarningGlyphicon').addClass("glyphicon-remove").css({"color": "red", "font-size": "100px", "opacity": "0.8"}).removeClass("glyphicon-ok");
+            $('#noWarningsHeader').hide();
+        }
 
-    if(flameSensorValue == 1){
-        $('#sensorWarningsTable #flameAlert').remove();
-        noErrors();
+        if(flameSensorValue == 1){
+            $('#sensorWarningsTable #flameAlert').remove();
+            noErrors();
+        }
     }
-}
 
 //If all sensors are ok.
-function noErrors(){
-    var length = $("#sensorWarningsTable tr").length;
-    if(length == 0){
-        $('#sensorWarningGlyphicon').addClass("glyphicon-ok").css({"color": "green", "font-size": "100px", "opacity": "0.8"}).removeClass("glyphicon-remove");
-        $('#noWarningsHeader').show();
+    function noErrors(){
+        var length = $("#sensorWarningsTable tr").length;
+        if(length == 0){
+            $('#sensorWarningGlyphicon').addClass("glyphicon-ok").css({"color": "green", "font-size": "100px", "opacity": "0.8"}).removeClass("glyphicon-remove");
+            $('#noWarningsHeader').show();
+        }
     }
-}
 
 // Function for reading the temperature value of the DHT11 sensor
-function tempReading(data) {
-    var tempSensorValue = data.Temperature;
-    var outTemp1 = data.Ds1Value;
-    var outTemp2 = data.Ds2Value;
-    $('#tempOut1').html(outTemp1+celsius);
-    $('#tempOut2').html(outTemp2+celsius);
-    if (tempSensorValue < 5) {
-        $('#tempValue').html(tempSensorValue + celsius).css("color", "#42a7f4");
+    function tempReading(data) {
+        var tempSensorValue = data.Temperature;
+        var outTemp1 = data.Ds1Value;
+        var outTemp2 = data.Ds2Value;
+        $('#tempOut1').html(outTemp1+celsius);
+        $('#tempOut2').html(outTemp2+celsius);
+        if (tempSensorValue < 5) {
+            $('#tempValue').html(tempSensorValue + celsius).css("color", "#42a7f4");
+        }
+        if (tempSensorValue > 5 && tempSensorValue < 30) {
+            $('#tempValue').html(tempSensorValue + celsius).css("color", "#40bf43");
+        }
+        if (tempSensorValue > 30) {
+            $('#tempValue').html(tempSensorValue + celsius).css("color", "#f46e41");
+        }
     }
-    if (tempSensorValue > 5 && tempSensorValue < 30) {
-        $('#tempValue').html(tempSensorValue + celsius).css("color", "#40bf43");
-    }
-    if (tempSensorValue > 30) {
-        $('#tempValue').html(tempSensorValue + celsius).css("color", "#f46e41");
-    }
-}
 
 // Function for reading the humidity value of the  DHT11 sensor
-function humReading(data) {
-    var humSensorValue = data.Humidity;
-    $('#humValue').html(humSensorValue+"%");
-}
+    function humReading(data) {
+        var humSensorValue = data.Humidity;
+        $('#humValue').html(humSensorValue+"%");
+    }
+})
