2a83dcd0e5640278f77a511fda907a78a7c1bcb3
marcouadro <markus.markussen@hotmail.com>
2017-05-27 17:56:47 +0200

    test

--- middlewares/alarmAndLuxController.js
+++ middlewares/alarmAndLuxController.js
@@ -167,7 +167,7 @@ module.exports = function (app, io) {
         var flame = sensorData.Flame;
         var leak = sensorData.LeakValue;
         //Thresholds
-        var gasThreshold = 600;
+        var gasThreshold = 800;
         var leakThreshold = 1000;
 
         if (gas > gasThreshold) {
